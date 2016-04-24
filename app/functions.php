e424864218d456efcbd9c121cba10a0861300e69
littleguga <fed777os@gmail.com>
2016-04-24 21:51:42 +0300

    rearrange project structure add class autoloader

---
+++ app/functions.php
@@ -0,0 +1,22 @@
+<?php
+
+class Functions{
+    /**
+     * @param $str
+     * Puts str to log file
+     */
+    public static function addLog($str)
+    {
+        $str = "[" . date('H:i:s') . "]: " . $str . "\n";
+        file_put_contents(Config::getFileLog(), $str, FILE_APPEND);
+    }
+
+    /**
+     * @param array $last
+     * Saves last posts' ids to .json file
+     */
+    public static function saveLast($last)
+    {
+        file_put_contents(Config::getFileLast(), json_encode($last));
+    }
+}
